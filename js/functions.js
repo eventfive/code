affff2654b217456309138eca2ea1203f9f65c12
Dennis <db@eventfive.de>
2013-04-24 14:04:37 +0200

    init 5

--- js/functions.js
+++ js/functions.js
@@ -3,22 +3,22 @@ var deviceReadyDeferred = jQuery.Deferred();
 var jqmReadyDeferred = jQuery.Deferred();
 
 
-// Phonegap init
+// PHONEGAP ///////////////////////////////////////////////////////
 document.addEventListener("deviceReady", deviceReady, false);
 function deviceReady() {		
 	deviceReadyDeferred.resolve();  // Wie oben benannt wird jQuery nun mitgeteilt, dass deviceReady() fertig ist
 }
 
 
-// jQuery init
+// JQUERY /////////////////////////////////////////////////////////
 jQuery(document).ready(function () {
   jqmReadyDeferred.resolve();   // Hier wird jQuery mitgeteilt, dass es selbst fertig ist
 });
 
 
+// PHONEGAP & JQUERY //////////////////////////////////////////////
 // Hier legen wir fest, dass sobald jQuery und PhoneGap bereit sind, die Funktion "doWhenBothFrameworksLoaded" aufgerufen wird
 jQuery.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
-
 // Ab hier können alle Funktionnen laufen, die jQuery UND Phonegap betreffen
 function doWhenBothFrameworksLoaded() {
 	alert('Ready');
