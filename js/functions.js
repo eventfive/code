e31efea02eaf7bf68503dfbc9994f706a2ccc883
Dennis <db@eventfive.de>
2013-05-13 15:06:38 +0200

    63

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,11 @@
-// Globale Variablen festlegen
+// Framework Variablen
 var deviceReadyDeferred = jQuery.Deferred();
 var jqmReadyDeferred = jQuery.Deferred();
-var loading = false;
+// Zeit in Millisekunden wird an jeden Request angehangen um ihn unique zu machen (Safari Bug)
+var date = new Date();
+var timestamp = date.getTime();
+// data Variablen
+var uuid = timestamp;
 
 
 // PHONEGAP ///////////////////////////////////////////////////////
@@ -14,15 +18,8 @@ function deviceReady() {
 // JQUERY /////////////////////////////////////////////////////////
 jQuery(document).ready(function () {
 	jqmReadyDeferred.resolve();   // Hier wird jQuery mitgeteilt, dass es selbst fertig ist
-	$('a[href="#welcome"]').on("click", function(event){
-		// Seitenwechsel abbrechen
-		event.preventDefault();
-		$.mobile.loading('show', { html: '<div class="loading-spinner"></div>' });
-		// Funktionen ausführen
-		alert("asdasd");
-		// Seitenwechsel manuell starten
-		$.mobile.navigate( "#welcome" );
-	});
+		
+	
 });
 
 
@@ -31,6 +28,9 @@ jQuery(document).ready(function () {
 jQuery.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
 // Ab hier können alle Funktionen laufen!
 function doWhenBothFrameworksLoaded() {
+	
+	// GeräteID auslesen
+	uuid = getName(device.uuid);
 
 	// Accordion animation
 	$('.listanimation').bind('expand', function () {
@@ -40,19 +40,32 @@ function doWhenBothFrameworksLoaded() {
 	});
 	
 	// START screen
-	if ( $('.ui-page').attr("id") == "start" &&  $('#start.ui-page').css("display") != "none" ) {
+	if ( $('#start.ui-page').css("display") != "none" ) {
 		$('.user').hide().delay(2000).fadeIn();
+		
+		$("#sendName").click(function() {
+			$.ajax({
+				type: "POST",
+				url: "http://kiste.eventfive.de/voivoi/func.php?option=sendName&unique=" + timestamp,
+				data: "&id=" + uuid + "&username=" + $('#username').val(),
+				beforeSend: function() { $.mobile.loading('show') },
+				//error: AjaxFailed,
+				complete: function() { $.mobile.navigate( "#welcome" ) },
+			});
+			function AjaxFailed(result) {
+				alert(result.status + ' ' + result.statusText);
+			}
+		});
 	}
 	
 	// MENU functions
 	$('a[href="#welcome"]').on("click", function(event){
 		// Seitenwechsel abbrechen
 		event.preventDefault();
-		$.mobile.loading('show', { html: "<div class='loader'></div>" });
+		$.mobile.loading('show');
 		// Funktionen ausführen
 		
 		// Seitenwechsel manuell starten
 		$.mobile.navigate( "#welcome" );
 	});
-	
 }
