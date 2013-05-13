4bf61e189dabc4cc5f467584f03293b150413554
Dennis <db@eventfive.de>
2013-05-13 17:43:10 +0200

    66

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,9 @@ var jqmReadyDeferred = jQuery.Deferred();
 // Zeit in Millisekunden wird an jeden Request angehangen um ihn unique zu machen (Safari Bug)
 var date = new Date();
 var timestamp = date.getTime();
-// data Variablen
+// Weitere Variablen
 var uuid = timestamp;
+var startAnimation = true;
 
 
 // PHONEGAP ///////////////////////////////////////////////////////
@@ -32,40 +33,48 @@ function doWhenBothFrameworksLoaded() {
 	// GeräteID auslesen
 	uuid = getName(device.uuid);
 
+
+	// START screen
+	if ( startAnimation == true ) {
+		$('.user').hide().delay(2000).fadeIn();
+		startAnimation = false;
+	}
+	$("#sendName").click(function() {
+		var username = $.trim($("#username").val());
+		if ( username.length > 0 ) {
+			$.ajax({
+				type: "POST",
+				dataType: "jsonp",
+				url: "http://kiste.eventfive.de/voivoi/func.php?option=sendName&unique=" + timestamp,
+				data: { id: uuid, username: username },
+				beforeSend: function() { $.mobile.loading('show') },
+				//error: AjaxFailed,
+				complete: goToWelcome,
+			});
+		}
+		else { $('.error.username').hide().html("Bitte nenn uns doch deinen Namen!").fadeIn(); }
+	});
+	
+	// MENU functions
+	$('a[href="#welcome"]').on("click", function(event){
+		// Seitenwechsel abbrechen
+		event.preventDefault();
+		goToWelcome;
+	});
+	
+	// Weitere Funktionen
 	// Accordion animation
 	$('.listanimation').bind('expand', function () {
 		$(this).children().slideDown(500);
 		}).bind('collapse', function () {
 			$(this).children().next().slideUp(500);
 	});
-	
-	// START screen
-	if ( $('#start.ui-page').css("display") != "none" ) {
-		$('.user').hide().delay(2000).fadeIn();
-		alert("ready");
-		$("#sendName").click(function() {
-			$.ajax({
-				type: "POST",
-				url: "http://kiste.eventfive.de/voivoi/func.php?option=sendName&unique=" + timestamp,
-				data: "&id=" + uuid + "&username=" + $('#username').val(),
-				beforeSend: function() { $.mobile.loading('show') },
-				//error: AjaxFailed,
-				complete: function() { $.mobile.navigate( "#welcome" ) },
-			});
-			function AjaxFailed(result) {
-				alert(result.status + ' ' + result.statusText);
-			}
-		});
-	}
-	
-	// MENU functions
-	$('a[href="#welcome"]').on("click", function(event){
-		// Seitenwechsel abbrechen
-		event.preventDefault();
+}
+
+function goToWelcome() {
 		$.mobile.loading('show');
 		// Funktionen ausführen
 		
 		// Seitenwechsel manuell starten
 		$.mobile.navigate( "#welcome" );
-	});
-}
+	}
