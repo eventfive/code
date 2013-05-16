b43ec6525072e573b513764109afdf447360003d
Dennis <db@eventfive.de>
2013-05-16 22:04:08 +0200

    76

--- js/functions.js
+++ js/functions.js
@@ -6,11 +6,10 @@ var deviceReadyDeferred = jQuery.Deferred();
 var jqmReadyDeferred = jQuery.Deferred();
 var appURL = "http://kiste.eventfive.de/asd/"
 var uuid, platform, osVersion;
-var eventID, eventTitle, eventDescription, eventDate, eventTime, eventLogo;
 var restartApp = true;
-var username;
+var username, eventID;
 // Dummy Daten für lokales testen
-var uuid = "0", platform = "Desktop", osVersion = "0";
+var uuid = "10", platform = "Desktop", osVersion = "0";
 
 
 // PHONEGAP ///////////////////////////////////////////////////////
@@ -34,6 +33,20 @@ jQuery(document).ready(function () {
 	// Kommentar Counter
 	$('#comment').NobleCount('#counter',{ max_chars: 140 });
 	
+	
+	
+			
+
+	// MENU functions
+	$('a[href="#welcome"]').on("click", function(event){
+		// Seitenwechsel abbrechen
+		// event.preventDefault();
+	});
+	$('a[href="#events"]').on("click", function(event){
+		/*event.preventDefault();
+		getEventsData();
+		$.mobile.navigate( "#events" );*/
+	});
 
 });
 
@@ -55,68 +68,12 @@ function doWhenBothFrameworksLoaded() {
 		getEventsData();
 		getUserData();
 		restartApp = false;
-	}		
-
-	// MENU functions
-	$('a[href="#welcome"]').on("click", function(event){
-		// Seitenwechsel abbrechen
-		// event.preventDefault();
-	});
-	$('a[href="#events"]').on("click", function(event){
-		/*event.preventDefault();
-		getEventsData();
-		$.mobile.navigate( "#events" );*/
-	});
+	}
+	
 	
 }
 
 /////// FUNKTIONEN //////////////////////
-
-function getUserData() {
-	$.ajax({
-		type: "GET",
-		contentType: "application/json",
-    	dataType: "JSONP",
-		crossDomain: true,
-		jsonp: 'jsoncallback',
-		url: appURL + "app.php?option=getUserData",
-		data: { userID: uuid, unique: timestamp },
-		cache: false,
-		success: function(data){
-			$.each(data, function(i,item){
-				username = item.username;
-				if ( username != null && username != "") {
-					$('span.username').html(username);
-					$.mobile.navigate( "#welcome" );
-				}
-				else $('#start .user').fadeIn();
-			});
-		},
-	});
-};
-	
-
-function sendName() {
-	username = $.trim($("input#username").val());
-	if ( username.length > 0 ) {
-		$.ajax({
-			type: "POST",
-			contentType: "application/json",
-    		dataType: "JSONP",
-			jsonp: 'jsoncallback',
-			url: appURL + "sendName" ,
-			data: { userID: uuid, platform: platform, osVersion: osVersion, username: username, unique: timestamp },
-			beforeSend: function() { $.mobile.loading('show') },
-			cache: false,
-			success: function() {
-				$('span.username').html(username);
-				$.mobile.navigate( "#welcome" );
-				}
-		});
-	}
-	else { $('.error.username').hide().html("Bitte nenn uns doch einen Namen").fadeIn(); }
-};
-
 
 function getEventsData() {
 	// Vorher 
@@ -134,10 +91,10 @@ function getEventsData() {
 		success: function(data){
 			$.each(data, function(i,item){
 				$('#eventCollapsible').append(
-				'<div data-role="collapsible" class="listanimation">' +
+				'<div data-role="collapsible" class="listanimation eventID-' + item.eventID +'">' +
 					'<h3>' + item.eventTitle + '</h3>' +
 					'<div class="description">' +
-						'<div class="logo"><img src="' + appURL + '/events/' + item.eventID + '/' + item.eventLogo + '" width="100%"/></div>' +
+						'<div class="logo"><img src="' + appURL + 'events/' + item.eventID + '/' + item.eventLogo + '" width="100%"/></div>' +
 						'<div class="time">' +
 							'<span class="date">' + item.eventDate + '</span>' +
                             '<span class="clock">' + item.eventTime + '</span>' +
@@ -153,3 +110,91 @@ function getEventsData() {
 		},
 	});
 };
+
+function getUserData() {
+	$.ajax({
+		type: "GET",
+		contentType: "application/json",
+    	dataType: "JSONP",
+		crossDomain: true,
+		jsonp: 'jsoncallback',
+		url: appURL + "app.php?option=getUserData",
+		data: { userID: uuid, unique: timestamp },
+		cache: false,
+		success: function(data){
+					if ( $.isEmptyObject(data) ) $('#start .user').fadeIn();
+					else { $.each(data, function(i,item) {
+								// Event auswählen
+								$('.eventID-' + item.selectedEventID + '.ui-collapsible .ui-btn-inner').addClass('selected');
+								// Kategorien laden
+								getEventCategories(item.selectedEventID);
+								// Username einsetzen
+								$('span.username').html(item.username);
+								// Weiterleitung
+								$.mobile.navigate( "#welcome" );
+							})
+					}
+			}
+	});
+};
+	
+
+function saveName() {
+	username = $.trim($("input#username").val());
+	if ( username.length > 0 ) {
+		$.ajax({
+			type: "POST",
+			contentType: "application/json",
+    		dataType: "JSONP",
+			jsonp: 'jsoncallback',
+			url: appURL + "app.php?option=saveName" ,
+			data: { userID: uuid, platform: platform, osVersion: osVersion, username: username, unique: timestamp },
+			beforeSend: function() { $.mobile.loading('show') },
+			cache: false,
+			success: function() {
+				$('span.username').html(username);
+				$.mobile.navigate( "#welcome" );
+				}
+		});
+	}
+	else { $('.error.username').hide().html("Bitte nenn uns doch einen Namen").fadeIn(); }
+};
+
+
+function selectEvent(eventID) {
+	$.ajax({
+		type: "POST",
+		contentType: "application/json",
+		dataType: "JSONP",
+		jsonp: 'jsoncallback',
+		url: appURL + "app.php?option=selectEvent" ,
+		data: { userID: uuid, eventID: eventID, unique: timestamp },
+		beforeSend: function() { $.mobile.loading('show') },
+		cache: false,
+		success: function() {
+			$('.ui-collapsible .ui-btn-inner').removeClass('selected');
+			$('.eventID-' + eventID + '.ui-collapsible .ui-btn-inner').addClass('selected');
+			$('.listanimation').trigger('collapse');
+			getEventCategories(eventID);
+			$.mobile.loading('hide');
+			}
+	});
+}
+
+function getEventCategories(eventID) {
+	$('.categoryOption').remove();
+	$.ajax({
+		type: "GET",
+		contentType: "application/json",
+		dataType: "JSONP",
+		jsonp: 'jsoncallback',
+		url: appURL + "app.php?option=getEventCategories" ,
+		data: { userID: uuid, eventID: eventID, unique: timestamp },
+		cache: false,
+		success: function(data) {
+					$.each(data, function(i,item) {
+						$('select#chooseCat').append('<option class="categoryOption" value="' + i + '">' + item.categoryTitle + '</option>');
+					});
+				}
+	});
+}
