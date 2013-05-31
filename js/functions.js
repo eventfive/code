e7fda885263468f05de1976ea62fcba0263abe9e
Dennis <db@eventfive.de>
2013-05-31 10:30:23 +0200

    115

--- js/functions.js
+++ js/functions.js
@@ -263,11 +263,11 @@ function getPictureGallery() {
 							);
 					});
 					// Lazyload aktivieren
-					$(".thumbnail img").lazyload( {
+					$(".imageWrapper").find("img").lazyload( {
 						effect: "fadeIn",
 					});
 					// Lazyload funktioniert erst ab dem SCROLL Event, also 1px scrollen damit die Bilder geladen werden
-					//$.mobile.silentScroll(1);
+					$.mobile.silentScroll(1);
 					// Popup aktivieren
 					$('.imageWrapper a').magnificPopup({ 
 					  type: 'image',
